3c3b8848c190718db8f3a5d38fc23f1a2638cc17
VuTrungNghia <vunghia467@gmail.com>
2024-07-05 18:17:08 +0700

    UpdateOrder

--- order-service/src/main/java/com/example/Microservice_order/service/OrderService.java
+++ order-service/src/main/java/com/example/Microservice_order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.Microservice_order.service;
 
+import com.example.Microservice_order.config.WebConfig;
 import com.example.Microservice_order.dto.DtoOrder;
 import com.example.Microservice_order.mapper.MapOrder;
 import com.example.Microservice_order.model.Order;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,18 +19,25 @@ import java.util.stream.Collectors;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final MapOrder mapOrder;
-
+    private final WebClient webClient;
     @Autowired
-    public OrderService(OrderRepository orderRepository, MapOrder mapOrder) {
+    public OrderService(OrderRepository orderRepository, MapOrder mapOrder, WebClient webClient) {
         this.orderRepository = orderRepository;
         this.mapOrder = mapOrder;
+        this.webClient = webClient;
     }
 
     @Transactional
     public Order createOrder(DtoOrder dtoOrder) {
         Order order = mapOrder.mapToOrder(dtoOrder);
-        order = orderRepository.save(order);
-        log.info("Order {} is saved", order.getId());
+        Boolean callresult = webClient.get().uri("http://localhost:8082/api/inventory").retrieve().bodyToMono(Boolean.class).block();
+        if(callresult){
+            order = orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Is not stock from produc, please try again");
+        }
+//        order = orderRepository.save(order);
+//        log.info("Order {} is saved", order.getId());
         return order;
     }
 
